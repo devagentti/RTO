ee9005307494224feaa0340bc566c79104a532c0
devagentti <41487382+devagentti@users.noreply.github.com>
2018-07-24 10:16:53 -0300

    Add files via upload

---
+++ core/Controller.php
@@ -0,0 +1,19 @@
+<?php
+class Controller {
+
+    public function loadView($viewName, $viewData = array()) {
+        extract($viewData); // Extrai o array transformando cada item do mesmo em um variável!! 
+        require 'views/'.$viewName.'.php';
+    }
+
+    public function loadTemplate($viewName, $viewData = array()) {
+        require 'views/template.php';
+    }
+
+    public function loadViewInTemplate($viewName, $viewData = array()) {
+        extract($viewData);
+        require 'views/'.$viewName.'.php';
+    }
+
+
+}
